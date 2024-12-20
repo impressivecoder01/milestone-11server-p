6cd8ce28fe1f7288b3db912b90133733144c4d3d
impressivecoder01 <rohanrahman251@gmail.com>
2024-12-21 04:22:24 +0600

    get api for get data from server

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // work related apis
+    const jobsCollections = client.db('milestone-11(p)').collection('works')
+
+    app.get('/works' , async(req, res)=> {
+        const cursor = jobsCollections.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
